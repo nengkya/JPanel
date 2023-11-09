ad30af686f292b94cf129ee866137b51b8b94da6
nengkya <nengkya@yahoo.com>
2023-11-09 17:11:47 +0700

    System.out.println(Double.MAX_VALUE);
    https://www.geeksforgeeks.org/generating-random-numbers-in-java/

--- MyJPanel.java
+++ MyJPanel.java
@@ -1,10 +1,17 @@
 import javax.swing.JPanel;
+import javax.swing.JFrame;
 
 public class MyJPanel {
 
     public static void main(String[] arguments) {
 
 	JPanel myJPanel = new JPanel();
+	double a = 3.14;
+	int b = (int)a;
+	int[] myInt = new int[22];
+	Integer ccc = 6;
+
+	System.out.println(Double.MAX_VALUE);
 
     }
 
