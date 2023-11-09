d8ab5f8b58700246658aaf3c31259687a5e9cd03
nengkya <nengkya@yahoo.com>
2023-11-09 21:32:16 +0700

    import java.util.Random;
     Random myRandom = new Random();

--- MyRandom.java
+++ MyRandom.java
@@ -8,8 +8,9 @@ public class MyRandom {
 	Random myRandom = new Random();
 
 	/* generate random integer in range 0 to 999 */
-	int randomInteger1 = myRandom.nextInt(1000);
-	int randomInteger2 = myRandom.nextInt(1000);
+	int randomInteger = myRandom.nextInt(1000);
+
+	System.out.println(randomInteger);
 
     }
 
